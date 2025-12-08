5ba606606d67b0a7e6d8bd0fc81212b49ff538fe
Lashan Gunawardena <ravijalashangunawardena@gmail.com>
2025-12-09 01:27:44 +0530

    Incomplete setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "../src/Pages/Auth/Login";
+import { AuthProvider } from "./AuthProvider/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+import AdminDashboard from "./Pages/Admin/AdminDashboard";
+import { AdminProtected, UserProtected } from "./Pages/Auth/ProtectedRoutes";
+import StudentLayout from "./Layout/StudentLayout";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const queryClient = new QueryClient();
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Navigate to="/login" replace />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/admin-dashboard"
+              element={
+                <AdminProtected>
+                  <AdminDashboard />
+                </AdminProtected>
+              }
+            />
 
-export default App
+            <Route
+              path="/dashboard"
+              element={
+                <UserProtected>
+                  <StudentLayout />
+                </UserProtected>
+              }
+            />
+            <Route path="*" element={<Navigate to="/login" replace />} />
+          </Routes>
+
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              style: {
+                background: "#363636",
+                color: "#fff",
+                borderRadius: "12px",
+                padding: "16px",
+                fontSize: "14px",
+                boxShadow: "0 10px 25px rgba(0, 0, 0, 0.2)",
+              },
+              success: {
+                style: {
+                  background: "#10B981",
+                },
+                iconTheme: {
+                  primary: "#fff",
+                  secondary: "#10B981",
+                },
+              },
+              error: {
+                style: {
+                  background: "#EF4444",
+                },
+                iconTheme: {
+                  primary: "#fff",
+                  secondary: "#EF4444",
+                },
+              },
+            }}
+          />
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+};
+
+export default App;
