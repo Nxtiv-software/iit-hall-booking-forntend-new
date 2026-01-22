d8c59b1e6e7e50ae4a72f311de4eb6238dcc35f6
Lashan Gunawardena <ravijalashangunawardena@gmail.com>
2026-01-22 05:32:18 +0530

    feat: enhance password reset component with loading state and UI improvements

--- src/Pages/Auth/Firebase-Auth/passwordReset.tsx
+++ src/Pages/Auth/Firebase-Auth/passwordReset.tsx
@@ -1,14 +1,28 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 import { auth } from "@/Firebase/config";
 import { sendPasswordResetEmail } from "firebase/auth";
+
 import { useState } from "react";
 import toast from "react-hot-toast";
 
 function PasswordReset() {
   const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleReset = async (e) => {
     e.preventDefault();
     try {
+      setLoading(true);
       await sendPasswordResetEmail(auth, email, {
         url: "https://iit-hallbooking.firebaseapp.com", 
       });
@@ -20,18 +34,37 @@ function PasswordReset() {
   };
 
   return (
-    <div>
-      <h2>Reset Password</h2>
-      <form onSubmit={handleReset}>
-        <input
-          type="email"
-          placeholder="Enter your email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Send Reset Email</button>
-      </form>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Reset Password</CardTitle>
+          <CardDescription>
+            Enter your email below to reset your password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleReset}>
+            <div className="flex flex-col gap-6 mb-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+            </div>
+            <div className="flex-col gap-2">
+              <Button type="submit" className="w-full" disabled={loading}>
+                {loading ? "Sending..." : "Send Reset Email"}
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
